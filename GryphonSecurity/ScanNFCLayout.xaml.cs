b62722c03754782494a060e63abab83b8041f196
Ydii <thomas_kragsberger@live.dk>
2016-02-15 12:15:18 +0100

    test

---
+++ GryphonSecurity/ScanNFCLayout.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace GryphonSecurity
+{
+    public partial class ScanNFCLayout : PhoneApplicationPage
+    {
+        public ScanNFCLayout()
+        {
+            InitializeComponent();
+        }
+    }
+}
