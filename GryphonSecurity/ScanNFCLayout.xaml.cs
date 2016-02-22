c92c69a7d860ef2e1511eed4613f29fd01efc92e
masas1990 <masas90@hotmail.com>
2016-02-22 11:37:01 +0100

    NFC scan lavet(Mangler at vide message type af chippen)

--- GryphonSecurity/ScanNFCLayout.xaml.cs
+++ GryphonSecurity/ScanNFCLayout.xaml.cs
@@ -7,14 +7,46 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using Windows.Networking.Proximity;
+using System.Diagnostics;
 
 namespace GryphonSecurity
 {
     public partial class ScanNFCLayout : PhoneApplicationPage
     {
+        private Windows.Networking.Proximity.ProximityDevice device;
+
         public ScanNFCLayout()
         {
             InitializeComponent();
+
+            initializeProximitySample();
+        }
+
+     
+private void scanButton_Click(object sender, RoutedEventArgs e)
+        {
+               
+
+                  device.SubscribeForMessage("NDEF", messageReceived);
+              //  Debug.WriteLine("Published Message. ID is {0}", Id);
+            
+
+        }
+        private void messageReceived(ProximityDevice sender,ProximityMessage message)
+        {
+          
+            textBlockTest.Text += "message modetaget: " + message.DataAsString;
+          //  Debug.WriteLine("Received from {0}:'{1}'", sender.DeviceId, message.DataAsString);
+        }
+
+        private void initializeProximitySample() {
+
+            device = Windows.Networking.Proximity.ProximityDevice.GetDefault();
+            if (device == null)
+                Debug.WriteLine("Failed to initialized proximity device.\n" +
+                                 "Your device may not have proximity hardware.");
+
         }
     }
 }
